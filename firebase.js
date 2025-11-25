796fb5d1e0de5092c6a7b44bf48a47a32cb8e956
Lisboa <kingeffects14@gmail.com>
2025-11-25 13:20:59 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,11 @@
-g
+const firebaseConfig = {
+  apiKey: "AIzaSyAaRl3DLS2ocGmx1yeJoXVasdg7zKQC2Zs",
+  authDomain: "mentoria-auth-871ad.firebaseapp.com",
+  projectId: "mentoria-auth-871ad",
+  storageBucket: "mentoria-auth-871ad.firebasestorage.app",
+  messagingSenderId: "433587361022",
+  appId: "1:433587361022:web:54f26b3bf8c764144541af",
+  measurementId: "G-QH4CEXWLEL"
+};
+
+firebase.initializeApp(firebaseConfig);
