93f339e55319fee41d997c1f4c400fabd53b30b9
Lisboa <kingeffects14@gmail.com>
2025-11-25 16:19:04 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,6 @@
 // AUTH.JS (Firebase intacto + Supabase corrigido)
 // =======================
 
-
 // ---------- Helpers ----------
 function escapeHtml(s){
   if(!s) return '';
@@ -40,12 +39,17 @@ function updateThemeButtons(){
   });
 }
 
-
 // ---------- Page Wiring ----------
 document.addEventListener('DOMContentLoaded', () => {
 
   Theme.load();
 
+  // ==========================
+  // SUPABASE CLIENT
+  // ==========================
+  const SUPABASE_URL = "https://xjmmgvbzfsgjltzggysv.supabase.co";
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbW1ndmJ6ZnNnamx0emdneXN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwOTI3MDAsImV4cCI6MjA3OTY2ODcwMH0.UpJk8za096938yDfFXiLaFF7fYdZfuKA5v1Wo4xSYG4";
+  const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
   // ------------------ LOGIN ------------------
   const loginForm = document.getElementById('loginForm');
